baa003e35659bd87ce13489fa4ed3f27e6722417
Aris <sk.aris.condez@gmail.com>
2021-10-03 19:27:56 -0400

    add js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,3 @@
-$(document).ready(function(){
-
-    $("a").dblclick(function(){
-        $(this).css("color", "black");
-    });
-
-});
-
 $( function (){
     $('[data-bs-toggle="tooltip"]').tooltip()
 } )
